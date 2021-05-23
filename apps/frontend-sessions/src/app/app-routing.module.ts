4d7ed777920dfb26ffb981f27502dc92b39447ba
Anthony Ku Ong <anthonykuong@gmail.com>
2021-05-23 13:27:27 -0400

    routing correction to profile

--- apps/frontend-sessions/src/app/app-routing.module.ts
+++ apps/frontend-sessions/src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {TranslateModule} from '@ngx-translate/core';
 import {TranslationRootModule} from './module/translation/translation-root.module';
+import {AppComponent} from "./app.component";
 
-export const routes: Routes = [{
-  path: '',
-  loadChildren: () => import('../../../../libs/profile-update/src/lib/profile-update.module').then(m => m.ProfileUpdateModule)
-}];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+    {
+    path: 'profile',
+    loadChildren: () => import('../../../../libs/profile-update/src/lib/profile-update.module').then(m => m.ProfileUpdateModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), TranslateModule, TranslationRootModule],
